9c02b11091d769db4fd7e2754985eca73396dad0
Dung-Pham <phamdunglili04012003@li04012003gmail.12003gmailcom>
2024-03-16 04:09:57 +0700

    change conectdb

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,26 +1,26 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-// const sequelize = new Sequelize(
-//     process.env.DB_DATABASE_NAME, 
-//     process.env.DB_USERNAME, 
-//     process.env.DB_PASSWORD,
-//     {
-//         host: process.env.DB_HOST,
-//         dialect: 'mysql',
-//         // logging: 'false',
-//         dialectOptions: {
-//             "ssl": {
-//                 "require": true,
-//                 "rejectUnauthorized": false
-//             }
-//         }
-//     }
-// )
-const sequelize = new Sequelize('tuvansuckhoe', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE_NAME, 
+    process.env.DB_USERNAME, 
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        // logging: 'false',
+        dialectOptions: {
+            "ssl": {
+                "require": true,
+                "rejectUnauthorized": false
+            }
+        }
+    }
+)
+// const sequelize = new Sequelize('tuvansuckhoe', 'root', null, {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// })
 
 
 let connectDB = async () => {
